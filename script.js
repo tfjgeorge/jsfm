bdab1a1edf8c3785f4b3bf1e299c262b5b6d3d57
Thomas George <tfjgeorge@gmail.com>
2013-03-02 20:17:00 +0100

    correct integer approximation

--- script.js
+++ script.js
@@ -89,7 +89,7 @@ $("#play").click(function() {
 	var data = instrument_fm(fp,fm,I*fm,m,t);
 	var dataInt = Array(data.length);
 	for (var i=0; i<data.length; i++) {
-		dataInt[i] = 127 + Math.floor(data[i] * 128);
+		dataInt[i] = 128 + Math.floor(data[i] * 127);
 	}
 	console.log(dataInt);
 
