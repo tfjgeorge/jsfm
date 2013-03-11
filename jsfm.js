4081e93ba7ba433ce3b1aa40a41ea7c798d8b649
Thomas George <tfjgeorge@gmail.com>
2013-03-11 20:44:06 +0100

    working webaudio api version

--- jsfm.js
+++ jsfm.js
@@ -1,59 +1,77 @@
-var playing = false;
 var context = new webkitAudioContext();
-var oscillator;
+var fp = 400,
+    fm = 600,
+    mod_index = 2 * fp;
 
+var play = function() {
+	audionodes.envelope.gain.value = 1;
+};
+
+var stop = function() {
+	audionodes.envelope.gain.value = 0;
+};
+
+var playing = false;
 $("#play_button").click(function() {
 	if (playing) 
 	{		
 		playing = false;
-		oscillator.stop(0);
+		stop();
 	}
 	else
 	{
-	oscillator = context.createOscillator();
-	playing = true;
-	var modulator = context.createOscillator();
-	var modGain = context.createGainNode();
-
-	oscillator.frequency.value = 300;
-	modulator.connect(modGain);
-	modGain.connect(oscillator.frequency);
-	modGain.gain.value = 1600;
-	modulator.frequency.value = 300;
-
-	oscillator.connect(context.destination);
-	oscillator.start(0);
-	modulator.start(0);
-	console.log(oscillator);
+		play();
+		playing = true;
 	}
+});
+$("input").change(function() {
+	fp = parseFloat($("#fp").val());
+	fm = parseFloat($("#fm").val());
+	mod_index = parseFloat($("#i").val()) * fp;
+	
+	var data = buffer_fp.getChannelData(0);
+	for (var i=0; i<512; i++) {
+		data[i] = fp;
+	}
+
+	data = buffer_fm.getChannelData(0);
+	for (var i=0; i<512; i++) {
+		data[i] = fm;
+	}
+	audionodes.modulation_index.gain.value = mod_index;
+	
 });
 
 
-var graph;
-var map = {};
-var init = function() {
-	// Read JSON
-	$.getJSON('default.json',function(data) {
-		graph = data;
-		for (var object in graph) {
-			map[graph[object]["id"]] = graph[object];
-		}
-		init_structure();
-	});
-};
-
 var audionodes = {};
-var init_structure = function() {
-	// Create all objects
-	for (var object in graph) {
-		audionodes[graph[object]]["id"] = 
-		switch(graph[object]["type"]) {
-			case "multiply":
 
-			break;
-		}
-	}
-}
+// Create all nodes
+audionodes.fm_node = context.createBufferSource();
+audionodes.fm_oscillator = context.createOscillator();
+audionodes.modulation_index = context.createGainNode();
+audionodes.fp_node = context.createBufferSource();
+audionodes.fp_oscillator = context.createOscillator();
+audionodes.envelope = context.createGainNode();
 
+// Connect all nodes together
+audionodes.fm_node.connect(audionodes.fm_oscillator.frequency);
+audionodes.fm_oscillator.connect(audionodes.modulation_index);
+audionodes.fp_node.connect(audionodes.fp_oscillator.frequency);
+audionodes.modulation_index.connect(audionodes.fp_oscillator.frequency);
+audionodes.fp_oscillator.connect(audionodes.envelope);
+audionodes.envelope.connect(context.destination);
 
-init();
+// Set up nodes
+audionodes.fm_oscillator.start(context.currentTime);
+audionodes.fp_node.start(context.currentTime);
+audionodes.fp_oscillator.start(context.currentTime);
+
+audionodes.modulation_index.gain.value = mod_index;
+audionodes.envelope.gain.value = 0;
+
+var buffer_fp = context.createBuffer(1,512,context.sampleRate);
+var buffer_fm = context.createBuffer(1,512,context.sampleRate);
+audionodes.fp_node.buffer = buffer_fm;
+audionodes.fp_node.loop = true;
+audionodes.fm_node.buffer = buffer_fm;
+audionodes.fm_node.loop = true;
